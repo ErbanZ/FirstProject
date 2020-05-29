0e7b6e918e642de33eff30bcc45d6cc656b8d734
Zlj <zlyj365@qq.com>
2020-05-30 00:14:14 +0800

    【首页-顶部功能栏（选择年份）】 https://www.tapd.cn/30359996/prong/stories/view/1130359996001002571

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import CurrentYear from './containers/CurrentYear';
 import Home from './containers/Home';
-import PastYear from './containers/PastYear';
 
 // 选择年份
 function Routes() {
@@ -10,11 +9,10 @@ function Routes() {
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/2020" component={CurrentYear} exact />
-      <Route path="/2019" component={PastYear} exact />
+      {/* <Route path="/2019" component={PastYear} exact />
       <Route path="/2018" component={PastYear} exact />
-      <Route path="/2017" component={PastYear} exact />
+      <Route path="/2017" component={PastYear} exact /> */}
     </Switch>
   );
 }
-
 export default Routes;
